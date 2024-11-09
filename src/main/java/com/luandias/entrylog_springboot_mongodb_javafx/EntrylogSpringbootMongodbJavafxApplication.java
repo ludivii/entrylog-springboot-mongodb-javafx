2dd752ecee8d9a0b3a18f767a120985675c74aab
Luan <luandsvt@gmail.com>
2024-11-09 14:17:03 -0300

    Project started

---
+++ src/main/java/com/luandias/entrylog_springboot_mongodb_javafx/EntrylogSpringbootMongodbJavafxApplication.java
@@ -0,0 +1,13 @@
+package com.luandias.entrylog_springboot_mongodb_javafx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrylogSpringbootMongodbJavafxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrylogSpringbootMongodbJavafxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luandias/entrylog_springboot_mongodb_javafx/EntrylogSpringbootMongodbJavafxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luandias.entrylog_springboot_mongodb_javafx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrylogSpringbootMongodbJavafxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
